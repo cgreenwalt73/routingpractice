3404568a4259f4154986b76eea1ef81e825eaf50
Cody Greenwalt <cgreenwalt73@gmail.com>
2022-07-31 20:00:21 -0400

    Learning routing with React Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route, useParams} from 'react-router-dom';
+
+const Home = props =>{
+  return(
+    <div>
+      <h1>Welcome</h1>
+    </div>
+  );
+}
+
+const DisplayValue = props =>{
+  const {value} = useParams();
+  return(
+    <h1>{value}</h1>
+  );
+}
+
+const DisplayStyledWord = props =>{
+  const {word} = useParams();
+  const {color1} = useParams();
+  const {color2} = useParams();
+  
+  if (isNaN(word)) {
+    return(
+      <div>
+        <h1 style={{ color:color1, backgroundColor:color2}} >{word}</h1>
+      </div>
+    );
+  }
+  else {
+    return(<Home/>);
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <Home/> } />
+          <Route path='/:value' element={ <DisplayValue/> } />
+          <Route path='/:word/:color1/:color2' element={ <DisplayStyledWord/> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
